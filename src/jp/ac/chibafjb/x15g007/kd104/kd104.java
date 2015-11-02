af0e6e20309164fa172ff6778558187e2bad87b5
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-02 14:51:54 +0900

    課題４だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd104/kd104.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd104;
+
+public class kd104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+	}
+
+}
