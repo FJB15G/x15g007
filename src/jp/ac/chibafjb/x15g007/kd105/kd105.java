7d5306b22ff86a9c28f51659a6c204fda3a4363b
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-02 15:07:25 +0900

    課題５だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd105/kd105.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd105;
+
+public class kd105 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("氏名：　海老原　健大（えびはら　けんた）");
+		System.out.println("わたしはえびです");
+		System.out.println("そう、えびです");
+	}
+
+}
