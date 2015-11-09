d75dae327915e2b140c5bf65b3be373ab52317d3
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:54:20 +0900

    テスト０２だよ

---
+++ src/jp/ac/chibafjb/x15g007/kdT02/kdT02.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kdT02;
+
+public class kdT02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=100;a<=102;a++)
+			System.out.printf("%d\n",a);
+	}
+
+}
