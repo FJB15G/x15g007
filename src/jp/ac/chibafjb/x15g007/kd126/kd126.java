9a59368c21118d37ca243ed63b5d1f6321c16377
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:44:06 +0900

    課題２６だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd126/kd126.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd126;
+
+public class kd126 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=0;a<3;a++)
+			System.out.printf("12345\n");
+	}
+
+}
