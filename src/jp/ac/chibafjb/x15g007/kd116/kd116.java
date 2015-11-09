58c896cd48d538b3062042b722c6c87b6eb3e525
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:07:08 +0900

    課題１６だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd116/kd116.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd116;
+
+public class kd116 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=0;a<2;a++)
+			System.out.printf("あいうえお　あいうえお　あいうえお\nかきくけこ　かきくけこ　かきくけこ\nさしすせそ　さしすせそ　さしすせそ\n");
+	}
+
+}
