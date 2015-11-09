4c27c5bbd0fe91a9daef70265e2454d1a2d75bfb
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 13:57:59 +0900

    課題１３だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd113/kd113.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd113;
+
+public class kd113 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<5;i++)
+			System.out.println("***");
+	}
+
+}
