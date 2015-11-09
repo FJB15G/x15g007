e1c0059a114e83157fa09782b138c5a0dcd5f00b
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:56:10 +0900

    テスト０３だよ

---
+++ src/jp/ac/chibafjb/x15g007/kdT03/T03.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kdT03;
+
+public class T03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=55;a<=70;a+=5)
+			System.out.printf("%d\n",a);
+	}
+
+}
