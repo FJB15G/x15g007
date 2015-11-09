1f97bddb78bc6a57187b461e3187c3694530b3d3
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:39:37 +0900

    課題２４だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd124/kd124.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd124;
+
+public class kd124 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=0;a<10;a++)
+			System.out.printf("☆");
+	}
+
+}
