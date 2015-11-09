7247f2b5d77d332ecb05011ba84a926babf70f6d
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 13:53:47 +0900

    課題１１だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd111/kd111.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g007.kd111;
+
+public class kd111 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n");
+	}
+
+}
