389220572f12b62db10f81c8709d057cbfde28c9
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 15:08:17 +0900

    課題３１だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd131/kd131.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd131;
+
+public class kd131 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		x = 10;
+		System.out.printf("%d",x);
+	}
+
+}
