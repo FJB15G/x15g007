55e756430100931171630bab63e2b44562a3ec77
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:00:08 +0900

    課題１４だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd114/kd114.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd114;
+
+public class kd114 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<5;i++)
+			System.out.println("***");
+	}
+
+}
