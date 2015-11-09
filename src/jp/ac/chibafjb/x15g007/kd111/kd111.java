2bdf0ac9bb95f41266050514dbaeab0b251d3a2f
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:09:14 +0900

    課題１１だよ（修正版）

--- src/jp/ac/chibafjb/x15g007/kd111/kd111.java
+++ src/jp/ac/chibafjb/x15g007/kd111/kd111.java
@@ -4,7 +4,9 @@ public class kd111 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n船橋　太郎\n");
+		int i;
+		for(i=0;i<10;i++)
+		System.out.println("船橋　太郎");
 	}
 
 }
