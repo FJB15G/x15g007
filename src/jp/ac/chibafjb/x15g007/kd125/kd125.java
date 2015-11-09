d683d667c6316d61d87f3380911b915aeab3ab26
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:42:05 +0900

    課題２５だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd125/kd125.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd125;
+
+public class kd125 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=10;a<=30;a+=10)
+			System.out.printf("%d *\n",a);
+	}
+
+}
