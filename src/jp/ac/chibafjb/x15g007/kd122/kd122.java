430c86088510fc97541e4088d529f356acaa34e5
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:35:58 +0900

    課題２２だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd122/kd122.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd122;
+
+public class kd122 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=0;a<10;a++)
+			System.out.printf("%d\n",a);
+	}
+
+}
