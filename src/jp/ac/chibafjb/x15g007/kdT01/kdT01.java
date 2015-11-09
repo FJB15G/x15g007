ee96f160f41fa66a4aca9b7691586112b5474994
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 14:52:15 +0900

    テスト０１だよ

---
+++ src/jp/ac/chibafjb/x15g007/kdT01/kdT01.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kdT01;
+
+public class kdT01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=0;a<3;a++)
+			System.out.printf("%d\n",a+1);
+	}
+
+}
