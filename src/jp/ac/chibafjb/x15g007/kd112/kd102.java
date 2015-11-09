9ea4268c8df6be90c34127e3a9cbc0251b48b0f3
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-09 13:54:06 +0900

    課題１２だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd112/kd102.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd112;
+
+public class kd102 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<=100;i++)
+			System.out.println("船橋城ビジネス専門学校");
+	}
+
+}
