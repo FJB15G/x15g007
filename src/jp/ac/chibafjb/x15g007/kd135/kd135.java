127ed34f497f99629fcf9f04c12a24c6b52422e3
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 14:01:43 +0900

    課題３５だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd135/kd135.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g007.kd135;
+
+public class kd135 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1 = "appple";
+		String str2 = "orange";
+		System.out.printf("str1 = %s\n",str1);
+		System.out.printf("str2 = %s\n",str2);
+		
+	}
+
+}
