ccbf4c8943c5d54c136f474869918e68a4d9c7bc
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 14:10:45 +0900

    課題３７だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd137/kd137.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g007.kd137;
+
+public class kd137 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		int c;
+		a = 100;
+		b = 1;
+		c = a;
+		a = b;
+		b = c;
+		System.out.printf("a = %d\n",a);
+		System.out.printf("b = %d\n",b);
+		System.out.printf("c = %d\n",c);
+	}
+
+}
