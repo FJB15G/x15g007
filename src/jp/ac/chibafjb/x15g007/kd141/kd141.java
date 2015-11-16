408a9dc70c5b3d77dcf0ab92890e8957c037eb9d
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 14:45:32 +0900

    課題４１だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd141/kd141.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g007.kd141;
+
+import java.util.Scanner;
+
+
+public class kd141 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner1 = new Scanner(System.in);
+		String s1 = scanner1.next();
+		scanner1.close();
+		System.out.printf("整数を1つ入力してください　-->"+s1+"\n");
+		System.out.printf("あなたの入力した数は"+s1+"です。\n");
+		
+	}
+
+}
