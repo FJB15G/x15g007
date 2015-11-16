9169b9b5c5bcb20eec01bd062173e35bba2a14a7
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 13:51:14 +0900

    課題３２だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd132/kd132.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g007.kd132;
+
+public class kd132 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		x = 10;
+		System.out.printf("x = %d\n",x);
+	}
+
+}
