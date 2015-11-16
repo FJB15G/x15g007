aada2db00468f3cb44a6573bd5f7d8cd58511fa6
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 13:57:58 +0900

    課題３４だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd134/kd134.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g007.kd134;
+
+public class kd134 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		int y;
+		x = 10;
+		y = 20;
+		y = x;
+		System.out.printf("x = %d\n",x);
+		System.out.printf("y = %d\n",y);
+	}
+
+}
