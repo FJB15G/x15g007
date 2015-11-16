5a37c95458335e48872e7300a9fd22586fcc8eeb
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 14:15:43 +0900

    課題３８だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd138/kd138.java
@@ -0,0 +1,23 @@
+package jp.ac.chibafjb.x15g007.kd138;
+
+public class kd138 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		int c;
+		int d;
+		a = 1;
+		b = 2;
+		c = 3;
+		d = a;
+		a = b;
+		b = c;
+		c = d;
+		System.out.printf("a = %d\n",a);
+		System.out.printf("b = %d\n",b);
+		System.out.printf("c = %d\n",c);
+	}
+
+}
