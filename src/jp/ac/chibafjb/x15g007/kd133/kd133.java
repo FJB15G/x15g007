3b8d84c30ebcd89de6aca967d14cadc5e08869cb
unknown <x15g007@PC504N008.chiba-fjb.ac.jp>
2015-11-16 13:54:27 +0900

    課題３３だよ

---
+++ src/jp/ac/chibafjb/x15g007/kd133/kd133.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g007.kd133;
+
+public class kd133 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		int y;
+		x = 5;
+		y = 10;
+		System.out.printf("x+y = %d",x+y);
+	}
+
+}
